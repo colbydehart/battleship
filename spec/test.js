4d4a63cbeaf0364b6f707063d6df4dc3fd66b955
Colby DeHart <colbydehart@gmail.com>
2014-11-20 15:33:07 -0600

    Implemented Gulp
    w/ bourbon, neat, jshint, scss, mocha, and webserver with livereload

---
+++ spec/test.js
@@ -0,0 +1,322 @@
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+describe('Something', function(){
+  it('should do something', function(){
+    var foo = 'bar';
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
